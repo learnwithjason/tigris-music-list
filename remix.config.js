2d25286502c6d96642650188e96d6aa7585041f2
Jason Lengstorf <jason@lengstorf.com>
2023-04-14 17:54:07 -0700

    feat: try fly for comparison on load perf/caching

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	ignoredRouteFiles: ['**/.*'],
-	server:
-		process.env.NETLIFY || process.env.NETLIFY_LOCAL
-			? './server.js'
-			: undefined,
-	serverBuildPath: '.netlify/functions-internal/server.js',
-	// appDirectory: "app",
-	// assetsBuildDirectory: "public/build",
-	// publicPath: "/build/",
 	future: {
 		v2_errorBoundary: true,
 		v2_meta: true,
