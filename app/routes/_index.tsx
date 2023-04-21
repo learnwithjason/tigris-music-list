35c575600d65873b3909a44da3b9cd08f2fb5194
Jason Lengstorf <jason@lengstorf.com>
2023-04-20 21:37:11 -0700

    feat: text search support

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,34 +1,35 @@
-import type { V2_MetaFunction } from '@remix-run/react';
 import type { LoaderArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+
+import { useParams } from '@remix-run/react';
 import { getArtists } from 'db/get-artists.server';
+import { getGenreFromParams } from '~/utils';
 import { GenrePicker } from '~/components/genre-picker';
 import { ArtistList } from '~/components/artist-list';
-import { Layout } from '~/components/layout';
-import styles from '../styles/main.css';
+import { Search } from '~/components/search';
 
-export const meta: V2_MetaFunction = () => {
-	return [
-		{ title: 'Music Picker · Tigris Data + Remix' },
-		{ tagName: 'link', rel: 'stylesheet', href: styles },
-	];
-};
-
-export async function loader({ params }: LoaderArgs) {
-	const genres = params.genre ? [params.genre] : undefined;
-	const data = await getArtists({ genres });
+export async function loader({ request, params }: LoaderArgs) {
+	const url = new URL(request.url);
+	const q = url.searchParams.get('q') || undefined;
+	const genre = getGenreFromParams(params);
+	const genres = genre ? [genre] : undefined;
+	const data = await getArtists({ genres, q });
 
 	return data;
 }
 
 export default function Index() {
-	const { genres } = useLoaderData();
+	const params = useParams();
+	const genre = getGenreFromParams(params);
 
 	return (
-		<Layout title="Check out these bands I like">
-			<GenrePicker genres={genres} expandable={true} />
+		<>
+			<Search />
+			<GenrePicker
+				expandable={typeof genre === 'undefined'}
+				clearable={typeof genre !== 'undefined'}
+			/>
 
 			<ArtistList />
-		</Layout>
+		</>
 	);
 }
