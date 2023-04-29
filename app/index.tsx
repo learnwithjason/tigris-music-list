4ffdb6a47d7e138ad4402126ac1141d769b2da30
Jason Lengstorf <jason@lengstorf.com>
2023-04-29 14:59:04 -0700

    refactor: move logic inside component

--- app/index.tsx
+++ app/index.tsx
@@ -5,16 +5,10 @@ import { Search } from '~/components/search';
 // TODO export a loader that returns all artist and genre data
 
 export default function Index() {
-	const genre = undefined;
-
 	return (
 		<>
 			<Search />
-			<GenrePicker
-				expandable={typeof genre === 'undefined'}
-				clearable={typeof genre !== 'undefined'}
-			/>
-
+			<GenrePicker />
 			<ArtistList />
 		</>
 	);
